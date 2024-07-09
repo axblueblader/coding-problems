d051763f43134a2884451777ae8585a6e21809a1
Nguyen Quoc Viet <nguyenqviet98@gmail.com>
2024-07-09 21:15:43 +0800

    leetcode 872

---
+++ leetcode/tree/872_leaf_similiar_tree.go
@@ -0,0 +1,42 @@
+package leetcode
+
+// https://leetcode.com/problems/leaf-similar-trees/
+
+func scoped() bool {
+	type TreeNode struct {
+		Left  *TreeNode
+		Right *TreeNode
+		Val   int
+	}
+
+	leafSimilar := func(root1 *TreeNode, root2 *TreeNode) bool {
+		sequence1 := []int{}
+		sequence2 := []int{}
+
+		var dfs func(*TreeNode, *[]int)
+		dfs = func(node *TreeNode, sequence *[]int) {
+			if node.Left == nil && node.Right == nil {
+				*sequence = append(*sequence, node.Val)
+				return
+			}
+			if node.Left != nil {
+				dfs(node.Left, sequence)
+			}
+			if node.Right != nil {
+				dfs(node.Right, sequence)
+			}
+		}
+		dfs(root1, &sequence1)
+		dfs(root2, &sequence2)
+		if len(sequence1) != len(sequence2) {
+			return false
+		}
+		for i := range sequence1 {
+			if sequence1[i] != sequence2[i] {
+				return false
+			}
+		}
+		return true
+	}
+	return leafSimilar(nil, nil)
+}
